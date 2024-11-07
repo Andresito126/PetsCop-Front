f1d40074a5528b5a7917ce40694bfa1e8587a992
Andresito126 <creed126ax@gmail.com>
2024-11-06 23:10:41 -0600

    Add: principal page added.

---
+++ src/app/users/profile-settings/profile-settings-page/profile-settings-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-settings-page',
+  templateUrl: './profile-settings-page.component.html',
+  styleUrl: './profile-settings-page.component.css'
+})
+export class ProfileSettingsPageComponent {
+
+}
--- src/app/users/profile-settings/profile-settings.module.ts
+++ src/app/users/profile-settings/profile-settings.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { FormProfileUserComponent } from './form-profile-user/form-profile-user.component';
 import { FormsModule } from '@angular/forms';
+import { ProfileSettingsPageComponent } from './profile-settings-page/profile-settings-page.component';
 
 @NgModule({
   declarations: [
-    FormProfileUserComponent
+    FormProfileUserComponent,
+    ProfileSettingsPageComponent
   ],
   imports: [
     CommonModule,
     FormsModule
   ],
   exports: [
-    FormProfileUserComponent
+    FormProfileUserComponent,
+    ProfileSettingsPageComponent
   ],
   providers: [
     DatePipe
