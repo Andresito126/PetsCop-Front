ab5050378258728de630764c5ee40a9be658c7be
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-11 21:29:20 -0600

    Registro e inicio de sesión casi terminados, API consumida

--- src/app/credentials/login/login.component.ts
+++ src/app/credentials/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { UsersAuthService } from '../services/users-auth.service';
+import { IUserCredentialsSerialization } from '../models/iuser-credentials-serialization';
+import { IloginUserSerialization } from '../models/ilogin-user-serialization';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,14 @@ import { UsersAuthService } from '../services/users-auth.service';
 })
 export class LoginComponent {
 
-  usersService = inject(UsersAuthService);
+  constructor(private userAuthServices: UsersAuthService){}
+
+  log_credentials: IloginUserSerialization = {
+    id_user: 0,
+    email: "",
+    password_user: "",
+    type_user: ""
+  }
 
   fields = [
     { label: 'Correo electrónico', type: 'email', name: 'email', placeholder: 'Ingresa tu correo electrónico', required: true, ngName:"hola" },
@@ -16,7 +25,16 @@ export class LoginComponent {
   ];
 
   async onLogin(data: any) {
-    const response = await this.usersService.login(data);
-    console.log(response);
+    // const response = await this.usersService.login(data);
+    // console.log(response);
+  }
+
+  login(){
+    this.userAuthServices.login(this.log_credentials).subscribe(
+      response => {
+        console.log("Respuesta del servidor:", response)
+      },
+      error => console.log("Error:", error)
+    );
   }
 }
--- src/app/credentials/models/ilogin-user-serialization.ts
+++ src/app/credentials/models/ilogin-user-serialization.ts
@@ -1,2 +1,6 @@
 export interface IloginUserSerialization {
+    id_user: number;
+    email: string;
+    password_user: string;
+    type_user: string;
 }
