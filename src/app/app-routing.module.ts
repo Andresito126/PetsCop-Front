9c3e00565a968e8ad0066b5ccb284be8d208ae7b
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-15 13:52:05 -0600

    Vista de perfil de un usuario, y Card con info de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './credentials/login/login.component';
 import { RegisterComponent } from './credentials/register/register.component';
 import { ProfileSettingsPageComponent } from './users/profile-settings/profile-settings-page/profile-settings-page.component';
 import { ChooseUserComponent } from './credentials/choose-user/choose-user.component';
+import { ViewProfileUserComponent } from './users/view-profile-user/view-profile-user.component';
 const routes: Routes = [
   {path:'usuario', component:ChooseUserComponent},
   {path:'usuario/registro', component:RegisterComponent},
   {path:'login', component:LoginComponent},
-  {path:'', component:ProfileSettingsPageComponent}
+  {path:'', component:ProfileSettingsPageComponent},
+  {path:'user_profile', component: ViewProfileUserComponent},
   ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
