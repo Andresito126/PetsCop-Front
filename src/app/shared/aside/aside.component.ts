e78f3aec63fc1e159f84cfc232de848fc1abdcc3
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-16 16:14:42 -0600

    Agregando método para obtener foto de usuario en el aside

--- src/app/shared/aside/aside.component.ts
+++ src/app/shared/aside/aside.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserConfigurationService } from '../../users/services/user-configuration.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-aside',
@@ -8,6 +10,42 @@ import { Component } from '@angular/core';
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.css'
 })
-export class AsideComponent {
+export class AsideComponent implements OnInit {
+  constructor(
+    private userConfigurationServices: UserConfigurationService,
+    private domSanitizer: DomSanitizer
+  ){}
 
+  imgProfile: any;
+
+  rol_user: string = "";
+  id_user: number = 0;
+
+  ngOnInit(): void {
+      this.getImgProfile();
+  }
+
+  getImgProfile(){
+    const getting_id = localStorage.getItem("id_user");
+    this.id_user = getting_id ? JSON.parse(getting_id) : 0;
+    const getting_rol = localStorage.getItem("rol");
+    this.rol_user = getting_rol ? JSON.parse(getting_rol) : "";
+    
+    if(this.rol_user === "Normal"){
+      
+      this.userConfigurationServices.getOwnProfile(this.id_user).subscribe(
+        response => {
+          console.log("Usuario retornado:", response);
+          this.userConfigurationServices.getOwnProfilePhoto(response.profile_picture).subscribe(
+            img => {
+              const imgObj = URL.createObjectURL(img);
+              this.imgProfile = this.domSanitizer.bypassSecurityTrustUrl(imgObj);
+            },
+            error => console.log("Error:", error)
+          );
+        },
+        error => console.log("Error:", error)
+      )
+    }
+  }
 }
