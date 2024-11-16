c4bdb535ec1b5900c668a5f5960f497b03455f93
Andresito126 <creed126ax@gmail.com>
2024-11-16 17:39:36 -0600

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { LoginComponent } from './credentials/login/login.component';
 import { RegisterComponent } from './credentials/register/register.component';
 import { ProfileSettingsPageComponent } from './users/profile-settings/profile-settings-page/profile-settings-page.component';
 import { CardHomeComponent } from './home/shared/card-home/card-home.component';
+import { DetailPostPageComponent } from './home/detail-post-page/detail-post-page.component';
 const routes: Routes = [
   {path:'registro', component:RegisterComponent},
   {path:'login', component:LoginComponent},
   {path:'user/configuration', component:ProfileSettingsPageComponent},
-  {path:'', component:CardHomeComponent}
+  {path:'', component:CardHomeComponent},
+  {path:'detallePost', component:DetailPostPageComponent}
+  
   ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardHomeComponent } from './shared/card-home/card-home.component';
+import { DetailPostPageComponent } from './detail-post-page/detail-post-page.component';
+import { CommentsComponent } from './shared/comments/comments.component';
+
 
 
 
 @NgModule({
   declarations: [
-    CardHomeComponent
+    CardHomeComponent,
+    DetailPostPageComponent,
+    CommentsComponent
   ],
   imports: [
     CommonModule
