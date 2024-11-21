9841aa869d2ac595d931821965a1aab0640b1c03
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-20 19:51:59 -0600

    Corrigiendo bug de tipo de publicación 'Encontrado'

--- src/app/mascotas-pages/encontrados-page/encontrados-page.component.ts
+++ src/app/mascotas-pages/encontrados-page/encontrados-page.component.ts
@@ -18,7 +18,7 @@ export class EncontradosPageComponent {
 
   getRecentPost(): void{
     console.log("Obtener posts recientes")
-    this.postServices.getRecentPostOfAType("Found").subscribe(
+    this.postServices.getRecentPostOfAType("Encontrado").subscribe(
       response => {
         console.log("It's ok!");
         this.posts = response;
@@ -29,7 +29,7 @@ export class EncontradosPageComponent {
 
   getOldPost(): void{
     console.log("Ontener posts antigüos")
-    this.postServices.getOldPostOfAType("Found").subscribe(
+    this.postServices.getOldPostOfAType("Encontrado").subscribe(
       response => {
         console.log("It's ok!");
         this.posts = response;
