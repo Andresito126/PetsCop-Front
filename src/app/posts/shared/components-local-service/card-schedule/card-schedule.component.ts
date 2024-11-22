12376a5640d95f5e81bdc748b734a937bb621bc3
Andresito126 <creed126ax@gmail.com>
2024-11-22 03:50:28 -0600

    Add:some styles

--- src/app/posts/shared/components-local-service/card-schedule/card-schedule.component.ts
+++ src/app/posts/shared/components-local-service/card-schedule/card-schedule.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class CardScheduleComponent {
 
+ 
+  
+    horarios = [
+      { dia: 'Lunes', horarioEntrada: '11:00', horarioSalida:'19:00' },
+      { dia: 'Martes', horarioEntrada: '11:00', horarioSalida:'19:00' },
+      { dia: 'Miércoles', horarioEntrada: '11:00', horarioSalida:'19:00' },
+      { dia: 'Jueves', horarioEntrada: '11:00', horarioSalida:'19:00' },
+      { dia: 'Viernes', horarioEntrada: '11:00', horarioSalida:'19:00' },
+      { dia: 'Sábado', horarioEntrada: '11:00', horarioSalida:'19:00' },
+      { dia: 'Domingo', horarioEntrada: '11:00', horarioSalida:'19:00' }
+    ];
+  
+  
+  
+  
+
 }
