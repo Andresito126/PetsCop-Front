fc17ae56e0191294a7f473161d3099436ad2e826
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-11-25 01:25:30 -0600

    Consumiendo API para marcar post de 'Perdido' como 'encontrado'

--- src/app/shared/modals/finded-modal/finded-modal.component.ts
+++ src/app/shared/modals/finded-modal/finded-modal.component.ts
@@ -1,24 +1,63 @@
 import { CommonModule } from '@angular/common';
-import { Component , Output, EventEmitter} from '@angular/core';
+import { Component , Output, EventEmitter, Input} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PostService } from '../../../users/services/post.service';
+import { PostsService } from '../../../posts/services/posts.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-finded-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './finded-modal.component.html',
   styleUrl: './finded-modal.component.css'
 })
 export class FindedModalComponent {
 
+  @Input() id_post: string | null = "";
+  message: string = "";
+
   petFindedModal: boolean = false; 
   @Output() close = new EventEmitter<void>();
   @Output() confirm = new EventEmitter<void>();
+
+  constructor(private postServices: PostsService){}
 
   closeFindedModal(): void {
     this.close.emit();
   }
 
   sendFindedModal(): void {
-    this.confirm.emit();
+    if(this.id_post && this.message){
+      this.postServices.pass_post_of_lost_to_found(this.id_post, this.message).subscribe(
+        response => {
+          console.log("Agradecimiento enviado:", response);
+          Swal.fire({
+            icon: "success",
+            title: "Post actualizado a 'encontrado'",
+            showConfirmButton: false,
+            timer: 2500
+          })
+          this.confirm.emit();
+        },
+        error => {
+          console.log("Error:", error)
+          Swal.fire({
+            icon: "error",
+            title: "Error al actualizar el post",
+            showConfirmButton: false,
+            timer: 2500
+          })
+        }
+      )
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Error al actualizar el post",
+        showConfirmButton: false,
+        timer: 2500
+      })
+    }
+    
   }
 }
