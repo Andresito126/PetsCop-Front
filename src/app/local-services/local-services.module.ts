5d1056700a7aa8b024a771bc157ad4251d59f2b1
Andresito126 <creed126ax@gmail.com>
2024-11-26 03:16:59 -0600

    Add:Add

--- src/app/local-services/local-services.module.ts
+++ src/app/local-services/local-services.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ServicesPageComponent } from './pages/services-page/services-page.component';
 import { LocalsPageComponent } from './pages/locals-page/locals-page.component';
 import { PostsModule } from '../posts/posts.module';
 
@@ -8,8 +7,7 @@ import { PostsModule } from '../posts/posts.module';
 
 @NgModule({
   declarations: [
-    ServicesPageComponent,
-    LocalsPageComponent
+    LocalsPageComponent,
   ],
   imports: [
     CommonModule,
